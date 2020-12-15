9496b7087ef7f24a1f28e10fe4604af1298bd766
xuyang.li <xuyangl@wicresoft.com>
2020-12-15 13:55:52 +0800

    Initial Commit

---
+++ NetworkTe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkTe
+//
+//  Created by Elliott on 2020/12/15.
+//  Copyright © 2020 ElliottTest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
